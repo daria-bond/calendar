4957303a7f38497a983027040df6d293694f461c
daria-bond <daria.bond16@yandex.ru>
2024-07-04 23:27:21 +0300

    init project

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app';
+import React, { ReactElement } from 'react';
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+    return (
+        <div>
+            <Component {...pageProps} />
+        </div>
+    );
+}
