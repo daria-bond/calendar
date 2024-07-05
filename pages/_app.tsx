06ff3bfc33e007dc17349dff5a480983f5b876ba
daria-bond <daria.bond16@yandex.ru>
2024-07-05 16:11:13 +0300

    add structure of project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,3 @@
-import type { AppProps } from 'next/app';
-import React, { ReactElement } from 'react';
-import '../styles/_normalizeReset.scss';
+import '../src/app/styles/_normalizeReset.scss';
 
-export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return (
-    <div>
-      <Component {...pageProps} />
-    </div>
-  );
-}
+export { default } from '../src/app/providers';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { CalendarPage } from '../src/pages/calendar';
 
 const Index = () => {
-  return <div>Hello world!</div>;
+  return <CalendarPage />;
 };
 
 export default Index;
