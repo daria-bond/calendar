7377469f41ea2785cbbff33d1431aeefdf9cc0fe
daria-bond <daria.bond16@yandex.ru>
2024-07-05 12:41:14 +0300

    add _normalizeReset styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import React, { ReactElement } from 'react';
+import '../styles/_normalizeReset.scss';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
